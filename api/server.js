ab7d7e39d032aa7b78b548cddbd201bfc5e87a52
Hitesh Sharma <hitesh.sharma_cs18@gla.ac.in>
2020-10-25 19:33:41 +0530

    update

---
+++ api/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const path = require('path');
+const server = express();
+var bodyParser = require('body-parser');
+ 
+server.use(bodyParser.urlencoded({
+    extended: true
+  }));
+  server.use(bodyParser.json());
+
+// EXPRESS SPECIFIC STUFF
+server.use('/static',express.static('static')); // serving static files
+server.use(express.urlencoded());
+
+//set the templates directory
+server.set('views', path.join(__dirname, '../templates')); 
+
+// settings to render html templates
+server.engine('html', require('swig').renderFile);
+server.set('view engine', 'html');
+
+
+server.get('/', (req, res) => { 
+    res.status(200).render('index.html')
+});
+
+module.exports = server;
