c2f375cca61c8ee5baf91e59845e3209c175b4af
Shubbhang <shubhang.verma_cs18@gla.ac.in>
2020-11-02 20:29:12 +0530

    initial commit

--- api/sign_up.js
+++ api/sign_up.js
@@ -13,20 +13,31 @@ router.get('/',(req,res) => {
 
 router.post('/',(req,res) => {
     const newUser = req.body;
-    bcrypt.genSalt(saltRounds, function(err, salt) {
-        bcrypt.hash(newUser['password'], salt, function(err, hash) {
-            newUser['password'] = hash;
-
-            Users.add(newUser).then(user => {
-                console.log(newUser);
-                res.status(200).redirect('/signin');
-            })
-            .catch(error => {
-                res.status(404).send("Something went wrong!!");
-            })
-            
-        });
-    });
+    Users.findByemail(newUser['email']).then(user => {
+        if(user){
+            res.status(200).redirect('/signup');
+        }
+        else{
+            bcrypt.genSalt(saltRounds, function(err, salt) {
+                bcrypt.hash(newUser['password'], salt, function(err, hash) {
+                    newUser['password'] = hash;
+        
+                    Users.add(newUser).then(user => {
+                        console.log(newUser);
+                        res.status(200).redirect('/signin');
+                    })
+                    .catch(error => {
+                        res.status(404).send("Something went wrong!!");
+                    })
+                    
+                });
+            });
+        }
+    })
+    .catch(error => {
+        res.status(500).send('some error');
+    })
+    
 });
 
 module.exports = router; 
