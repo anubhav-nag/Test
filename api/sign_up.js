fc04115b3f6958c6f6dc8418de97c25df762a928
Hitesh Sharma <hitesh.sharma_cs18@gla.ac.in>
2020-11-03 11:56:12 +0530

    authentication

--- api/sign_up.js
+++ api/sign_up.js
@@ -1,7 +1,9 @@
 const { json } = require('body-parser');
 const express = require('express');
-
+const bcrypt = require('bcrypt');
 const router = express.Router();
+
+const saltRounds = 10;
 
 const Users = require('../models/dbHelper');
 
@@ -11,13 +13,41 @@ router.get('/',(req,res) => {
 
 router.post('/',(req,res) => {
     const newUser = req.body;
-    Users.add(newUser).then(user => {
-        console.log(newUser);
-        res.status(200).redirect('/signin');
+    Users.findByemail(newUser['email']).then(user => {
+        if(user){
+            res.status(200).redirect('/signup');
+        }
+        else{
+            bcrypt.genSalt(saltRounds, function(err, salt) {
+                bcrypt.hash(newUser['password'], salt, function(err, hash) {
+                    newUser['password'] = hash;
+        
+                    Users.add(newUser).then(user => {
+                        console.log(newUser);
+                        res.status(200).redirect('/signin');
+                    })
+                    .catch(error => {
+                        res.status(404).send("Something went wrong!!");
+                    })
+                    
+                });
+            });
+        }
     })
     .catch(error => {
-        res.status(404).send("Something went wrong!!");
+        res.status(500).send('some error');
     })
+    
 });
 
 module.exports = router; 
+
+
+
+// Users.add(newUser).then(user => {
+//     console.log(newUser);
+//     res.status(200).redirect('/signin');
+// })
+// .catch(error => {
+//     res.status(404).send("Something went wrong!!");
+// })
