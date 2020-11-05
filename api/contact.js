06a3af9749f45d5c659fd3c6bebb0c6ef3c76d29
Shubbhang <shubhang.verma_cs18@gla.ac.in>
2020-11-05 22:04:48 +0530

    initial commit

--- api/contact.js
+++ api/contact.js
@@ -2,8 +2,21 @@ const express = require('express');
 
 const router = express.Router();
 
+const User = require('../models/dbHelper');
+
 router.get('/',(req,res) => {
-    res.status(200).render('contact.html');
+    const id = req.session.userId;
+    if(id){
+        console.log(`*** ${id} ***`);
+        User.findByid(id).then(authUser => {
+            const user = {id : authUser.id, name : authUser.first_name + authUser.last_name, email : authUser.email}
+            res.status(200).render('contact.html',{user});
+        })
+    }
+    else{
+        console.log('no session');
+        res.status(200).render('contact.html');
+    }
 })
 
 module.exports = router;
