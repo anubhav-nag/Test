037e6778d87acf845a18bc82906dd35f51d48247
Shubbhang <shubhang.verma_cs18@gla.ac.in>
2020-11-06 09:40:10 +0530

    initial commit

--- api/server.js
+++ api/server.js
@@ -54,6 +54,11 @@ server.use(session({
   }
 }))
 
+server.use(function(req, res, next) {
+  res.set('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0');
+  next();
+});
+
 
 
 //flash messages
