8285e35e8852db1a30f7da923e63d08d89cb7d34
anubhav-nag <anubhav.nag_cs18@gla.ac.in>
2020-11-25 15:53:12 +0530

    added fuction to add products in the cart

--- static/js/cart.js
+++ static/js/cart.js
@@ -1,1 +1,31 @@
-alert("ok");
+function add_to_cart(pid,pname,price)
+{
+    let cart = localStorage.getItem("cart");  //empty cart
+
+    if(cart == null)
+    {
+        let products = [];
+        let product = { productId:pid, productName:pname , productQuantity:1 , productPrice:price }
+        products.push(product);
+        localStorage.setItem("cart",JSON.stringify(products));
+    }
+    else
+    {
+        //cart is already present and not empty
+        let pcart = JSON.parse(cart);
+        
+        let oldproduct = pcart.find((item) => item.productId == pid);
+        if(oldproduct)
+        {
+            //if item is already present
+            
+        }
+        else
+        {
+            //if item is not present
+            let product = { productId:pid, productName:pname , productQuantity:1 , productPrice:price }
+            pcart.push(product);
+            localStorage.setItem("cart",JSON.stringify(pcart));
+        }
+    }
+}
